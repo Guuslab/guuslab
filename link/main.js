c6c695f1d0e8721e82dbb52a95f4f349a3740fb3
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-06-19 13:39:32 +0200

    loading screen + hijskraan

--- link/main.js
+++ link/main.js
@@ -9,25 +9,59 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff);
 document.body.appendChild(renderer.domElement);
 
+const loadingScreen = document.getElementById('loading-screen');
+
 const container = document.getElementById('hallo');
+container.style.opacity = '0'; // Stel de initiële zichtbaarheid in op 0
 container.appendChild(renderer.domElement);
 
 const loader = new GLTFLoader();
 
+let model; // Variabele om het geladen model bij te houden
+
 loader.load(
-    'public/3dmodel/hijskraan.glb', // Replace with the path to your GLB file
+    'public/3dmodel/hijskraan.glb',
     function (gltf) {
-        const model = gltf.scene;
+        model = gltf.scene;
         scene.add(model);
+        model.scale.set(1.7, 1.7, 1.7);
+
+        // Animatie voor het vervagen van het laadscherm
+        let opacity = 0;
+        const fadeInInterval = setInterval(() => {
+            opacity += 0.01;
+            container.style.opacity = opacity;
+            if (opacity >= 1) {
+                clearInterval(fadeInInterval);
+                setTimeout(() => {
+                    loadingScreen.style.display = 'none'; // Verberg het laadscherm na een vertraging
+                }, 10000); // Pas de vertraging hier aan (in milliseconden)
+            }
+        }, 10);
     },
-    undefined,
+    function (xhr) {
+        // Bereken de voortgang van het laden
+        const progress = xhr.loaded / xhr.total;
+
+        // Werk de voortgangsbalk bij
+        const loadingProgress = document.getElementById('loading-progress');
+        loadingProgress.style.width = `${progress * 100}%`;
+    },
     function (error) {
         console.error(error);
     }
 );
 
-const light = new THREE.PointLight(0xffffff, 1);
-light.position.set(5, 3, -5);
+loader.manager.onLoad = function () {
+    // Model is volledig geladen
+    const loadingScreenFadeOutInterval = setInterval(() => {
+        loadingScreen.classList.add('fade-out');
+        clearInterval(loadingScreenFadeOutInterval);
+    }, 1000);
+};
+
+const light = new THREE.DirectionalLight(0xffffff, 1); // Gebruik DirectionalLight in plaats van PointLight
+light.position.set(0, 0, 1); // Stel de richting van het licht in (van voren)
 scene.add(light);
 
 camera.position.z = 5;
@@ -35,21 +69,21 @@ camera.position.z = 5;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-let scrollY = 0; // Variable to store the scroll position
+let scrollY = 0; // Variabele om de scrollpositie bij te houden
 
 function handleScroll() {
     scrollY = window.scrollY;
 }
 
-window.addEventListener('scroll', handleScroll); // Listen for scroll events
+window.addEventListener('scroll', handleScroll); // Luister naar scroll-gebeurtenissen
 
 function animate() {
     requestAnimationFrame(animate);
 
-    // Update the rotation based on scroll position
-    if (scene.children.length > 0) {
-        const model = scene.children[0];
-        model.rotation.y = scrollY * 0.01;
+    // Update de rotatie en positie op basis van de scrollpositie
+    if (model) {
+        model.rotation.x = scrollY * 0.0005;
+        model.position.y = -scrollY * 0.001;
     }
 
     renderer.render(scene, camera);
