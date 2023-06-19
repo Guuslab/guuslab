5298cae43e7c722ce559d2bbc9b7c20bb9a56ed7
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-06-19 14:59:34 +0200

    nice

--- link/main.js
+++ link/main.js
@@ -10,58 +10,56 @@ renderer.setClearColor(0xffffff);
 document.body.appendChild(renderer.domElement);
 
 const loadingScreen = document.getElementById('loading-screen');
-
+const loadingProgress = document.getElementById('loading-progress');
+const loadingText = document.getElementById('loading-text');
 const container = document.getElementById('hallo');
-container.style.opacity = '0'; // Stel de initiële zichtbaarheid in op 0
+container.style.opacity = '0';
 container.appendChild(renderer.domElement);
 
 const loader = new GLTFLoader();
 
-let model; // Variabele om het geladen model bij te houden
+let model;
 
 loader.load(
-    'public/3dmodel/hijskraan.glb',
-    function (gltf) {
-        model = gltf.scene;
-        scene.add(model);
-        model.scale.set(1.7, 1.7, 1.7);
+  'public/3dmodel/hijskraan.glb',
+  function (gltf) {
+    model = gltf.scene;
+    scene.add(model);
+    model.scale.set(1.7, 1.7, 1.7);
 
-        // Animatie voor het vervagen van het laadscherm
-        let opacity = 0;
-        const fadeInInterval = setInterval(() => {
-            opacity += 0.01;
-            container.style.opacity = opacity;
-            if (opacity >= 1) {
-                clearInterval(fadeInInterval);
-                setTimeout(() => {
-                    loadingScreen.style.display = 'none'; // Verberg het laadscherm na een vertraging
-                }, 10000); // Pas de vertraging hier aan (in milliseconden)
-            }
-        }, 10);
-    },
-    function (xhr) {
-        // Bereken de voortgang van het laden
-        const progress = xhr.loaded / xhr.total;
+    let opacity = 0;
+    const fadeInInterval = setInterval(() => {
+      opacity += 0.01;
+      container.style.opacity = opacity;
+      if (opacity >= 1) {
+        clearInterval(fadeInInterval);
+        setTimeout(() => {
+          loadingScreen.style.display = 'none';
+        }, 10000);
+      }
+    }, 10);
+  },
+  function (xhr) {
+    const progress = xhr.loaded / xhr.total;
 
-        // Werk de voortgangsbalk bij
-        const loadingProgress = document.getElementById('loading-progress');
-        loadingProgress.style.width = `${progress * 100}%`;
-    },
-    function (error) {
-        console.error(error);
-    }
+    const percentage = Math.round(progress * 100);
+    loadingProgress.style.width = `${percentage}%`;
+    loadingText.innerText = `${percentage}%`; // Voeg percentage toe aan tekst
+  },
+  function (error) {
+    console.error(error);
+  }
 );
 
 loader.manager.onLoad = function () {
-    // Model is volledig geladen
-    const loadingScreenFadeOutInterval = setInterval(() => {
-        loadingScreen.classList.add('fade-out');
-        clearInterval(loadingScreenFadeOutInterval);
-    }, 1000);
+  const loadingScreenFadeOutInterval = setInterval(() => {
+    loadingScreen.classList.add('fade-out');
+    clearInterval(loadingScreenFadeOutInterval);
+  }, 1000);
 };
 
-const light = new THREE.DirectionalLight(0xffffff, 1); // Gebruik DirectionalLight in plaats van PointLight
-light.position.set(0, 0, 1); // Stel de richting van het licht in (van voren)
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
 scene.add(light);
 
 camera.position.z = 5;
@@ -69,24 +67,23 @@ camera.position.z = 5;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-let scrollY = 0; // Variabele om de scrollpositie bij te houden
+let scrollY = 0;
 
 function handleScroll() {
-    scrollY = window.scrollY;
+  scrollY = window.scrollY;
 }
 
-window.addEventListener('scroll', handleScroll); // Luister naar scroll-gebeurtenissen
+window.addEventListener('scroll', handleScroll);
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    // Update de rotatie en positie op basis van de scrollpositie
-    if (model) {
-        model.rotation.x = scrollY * 0.0005;
-        model.position.y = -scrollY * 0.001;
-    }
+  if (model) {
+    model.rotation.x = scrollY * 0.0005;
+    model.position.y = -scrollY * -0.0003;
+  }
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 animate();
