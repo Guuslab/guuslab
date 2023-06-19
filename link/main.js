228f6398daf1acb8e594e5127cdb5a13ae8b38ab
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-06-19 15:20:10 +0200

    web app

--- link/main.js
+++ link/main.js
@@ -87,3 +87,21 @@ function animate() {
 }
 
 animate();
+
+
+
+let userAgent = navigator.userAgent;
+if (userAgent.match(/iPhone/i)) {
+    let css = '.nav {padding-top: 120px;}';
+    let head = document.head || document.getElementsByTagName('head')[0];
+    let style = document.createElement('style');
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
+}
