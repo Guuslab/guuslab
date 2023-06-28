a78ce1816a8cb82f6a548917f4a132248e537c25
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-06-28 18:58:15 +0200

    fix

--- link/main.js
+++ link/main.js
@@ -105,3 +105,27 @@ animate();
 
 //     head.appendChild(style);
 // }
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    if ("Notification" in window) {
+        // Ondersteuning voor desktopmeldingen (Mac)
+        Notification.requestPermission().then(perm => {
+            if (perm === "granted") {
+                const notifi = new Notification("test! test! test!", {
+                    body: "this is more text",
+                    data: {hallo: "world"},
+                    icon: "logob.svg"
+                });
+            }
+        });
+    } else if ("PushManager" in window) {
+        // Ondersteuning voor web-pushmeldingen (iPhone)
+        // Implementeer hier het Web Push-protocol en gebruik een service worker
+        // om meldingen naar iPhones te sturen.
+    } else {
+        // Meldingen worden niet ondersteund
+        console.log("Meldingen worden niet ondersteund in deze browser.");
+    }
+});
--- test/notifi.js
+++ test/notifi.js
@@ -1,13 +1,23 @@
-const button = document.querySelector("button")
+const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    Notification.requestPermission().then(perm => {
-        if (perm === "granted") {
-            const notifi = new Notification("test! test! test!", {
-                body: "this is more text",
-                data: {hallo: "world"},
-                icon: "logob.svg"
-            })
-        }
-    })
-})
+    if ("Notification" in window) {
+        // Ondersteuning voor desktopmeldingen (Mac)
+        Notification.requestPermission().then(perm => {
+            if (perm === "granted") {
+                const notifi = new Notification("test! test! test!", {
+                    body: "this is more text",
+                    data: {hallo: "world"},
+                    icon: "logob.svg"
+                });
+            }
+        });
+    } else if ("PushManager" in window) {
+        // Ondersteuning voor web-pushmeldingen (iPhone)
+        // Implementeer hier het Web Push-protocol en gebruik een service worker
+        // om meldingen naar iPhones te sturen.
+    } else {
+        // Meldingen worden niet ondersteund
+        console.log("Meldingen worden niet ondersteund in deze browser.");
+    }
+});
