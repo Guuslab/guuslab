89b984c0b64ac1a024a1201ce0e59774a1aaebe6
Guus Kaashoek <Guus.kaashoek@gmail.com>
2024-01-30 17:38:13 +0100

    Update navigation styles

--- js/font.js
+++ js/font.js
@@ -7,21 +7,25 @@
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
 
-  var text = document.querySelector('.hero-text');
+var text = document.querySelector('.hero-text');
 
-  function blink() {
-    text.style.textShadow = 'none';
-    // text.style.fontWeight = '100';
-    setTimeout(function() {
-      text.style.textShadow = '0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 30px #ffffff, 0 0 40px #ffffff, 0 0 50px #ffffff, 0 0 60px #ffffff';
-      // text.style.fontWeight = '900';
-    }, 100);
-  }
+function blink() {
+  text.style.textShadow = '0 0 50px #ffffff, 0 0 100px #ffffff, 0 0 150px #ffffff, 0 0 200px #ffffff, 0 0 250px #ffffff, 0 0 300px #ffffff';
+  setTimeout(function() {
+    text.style.textShadow = '0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 30px #ffffff, 0 0 40px #ffffff, 0 0 50px #ffffff, 0 0 60px #ffffff';
+  }, 100);
+}
 
-  for (var i = 0; i < 7; i++) {
-    setTimeout(blink, Math.random() * 2000);
-  }
+for (var i = 0; i < 5; i++) {
+  setTimeout(blink, Math.random() * 1500);
+}
 
-  document.querySelector('.sky-img').addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-  });
+setTimeout(function() {
+    text.style.filter = 'blur(0px)';
+    text.style.textShadow = '0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 30px #ffffff, 0 0 40px #ffffff, 0 0 50px #ffffff, 0 0 60px #ffffff';
+}, 1000);
+
+
+  // document.querySelector('.sky-img').addEventListener('contextmenu', function(e) {
+  //   e.preventDefault();
+  // });
