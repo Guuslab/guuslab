2f946b3261bb54c37840994846197c803672e884
Guus Kaashoek <Guus.kaashoek@gmail.com>
2024-04-03 19:28:05 +0200

    Add new scripts and update API.js

--- js/api.js
+++ js/api.js
@@ -1,11 +1,22 @@
-window.onload = function() {
+function fetchWord() {
     fetch('https://api.guuslab.com/api/word?lan=nl')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
-            var heroElement = document.querySelector('.hero-q');
-            if (data.words && data.words.length > 0) {
-                heroElement.textContent = data.words[0];
+            var heroElement = document.querySelector('#random');
+            if (heroElement) {
+                if (data.words && data.words.length > 0) {
+                    heroElement.textContent = data.words[0];
+                }
+            } else {
+                console.error('Element with id "random" not found');
             }
         })
         .catch(error => console.error('Error:', error));
-};
+}
+
+fetchWord(); // Call the function immediately
